e02e74f25d28fa81fe32d13219f3a5243a6b895e
max2554884 <max.wurmbach@t-online.de>
2025-03-28 10:36:17 +0100

    neu

--- Benutzerverwaltung.java
+++ Benutzerverwaltung.java
@@ -1,2 +1,36 @@
-public class Benutzerverwaltung {
+import utils.*;
+/**
+ * Beschreiben Sie hier die Klasse Benutzerverwaltung.
+ *
+ * @author (Ihr Name)
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Benutzerverwaltung
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private BinarySearchTree<Benutzerprofil> benutzerBaum;
+
+    /**
+     * Konstruktor für Objekte der Klasse Benutzerverwaltung
+     */
+    public Benutzerverwaltung()
+    {
+        // Instanzvariable initialisieren
+        benutzerBaum = new BinarySearchTree();
+    }
+
+    public void neuenNutzerAnlegen(String pBenutzername, String pPw)
+    {
+
+    }
+
+    public void benutzerLoeschen(String pBenutzername, String pPw)
+    {
+
+    }
+
+    public void profilVorhanden(String pBenutzername)
+    {
+
+    }
 }
